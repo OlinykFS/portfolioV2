8db220d5248d839b75c25d6f39978076b4a6f383
Oleh Olinyk <oleh.olinyk.oo@gmail.com>
2024-08-25 16:04:16 +0200

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'bg-body': '#0F172A'
+        "bg-body": "#0F172A",
+      },
+      fontFamily: {
+        inter: [
+          "var(--font-inter)",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
       },
     },
   },
