3839c2a79cfed967678641feba54512395fd8909
Oleh Olinyk <oleh.olinyk.oo@gmail.com>
2024-08-26 22:00:48 +0200

    upd
    font configure

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,37 @@ module.exports = {
         "bg-body": "#0F172A",
       },
       fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
-      },
-      fontFeatureSettings: {
-        default: ['"ss03"', '"cv02"', '"cv11"'],
+        sans: [
+          "Inter var",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        html: {
+          fontFeatureSettings: '"ss03", "cv02", "cv11"',
+          fontVariationSettings: "normal",
+          lineHeight: "1.5",
+          textSizeAdjust: "100%",
+          tabSize: "4",
+        },
+      });
+    },
+  ],
 };
