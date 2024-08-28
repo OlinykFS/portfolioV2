1f16c00e4e6fdf6d1cf4fb2128369636672ec450
Oleh Olinyk <oleh.olinyk.oo@gmail.com>
2024-08-28 19:46:51 +0200

    upd
    +header section

--- js/script.js
+++ js/script.js
@@ -6,3 +6,35 @@ document.addEventListener("mousemove", (e) => {
 
   gradientElement.style.background = `radial-gradient(600px at ${x}px ${y}px, rgba(29, 78, 216, 0.15), transparent 80%)`;
 });
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll("nav a");
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.top <=
+      (window.innerHeight || document.documentElement.clientHeight) * 0.5
+  );
+}
+
+function updateActiveLink() {
+  let currentSectionId = "";
+
+  sections.forEach((section) => {
+    if (isElementInViewport(section)) {
+      currentSectionId = section.id;
+    }
+  });
+
+  navItems.forEach((item) => {
+    item.classList.remove("active");
+    if (item.getAttribute("href") === `#${currentSectionId}`) {
+      item.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", updateActiveLink);
+window.addEventListener("resize", updateActiveLink);
+document.addEventListener("DOMContentLoaded", updateActiveLink);
